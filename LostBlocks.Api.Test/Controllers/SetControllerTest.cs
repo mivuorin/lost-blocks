f8555da8de93011114e1e24b2a9d4fb6cce4a27d
mivuorin <mikko.h.vuorinen@gmail.com>
2025-04-15 06:40:37 +0300

    Setup test data in SetController tests

--- LostBlocks.Api.Test/Controllers/SetControllerTest.cs
+++ LostBlocks.Api.Test/Controllers/SetControllerTest.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using LostBlocks.Api.Controllers;
 using LostBlocks.Api.Models;
+using LostBlocks.Api.Test.AutoFixture;
 using Xunit;
 
 namespace LostBlocks.Api.Test.Controllers;
@@ -14,11 +15,15 @@ public class SetControllerTest : DatabaseTest
         controller = new SetController(Context);
     }
 
-    [Fact]
-    public async Task Return_all_sets_by_theme()
+    [Theory]
+    [LegoAutoData]
+    public async Task Return_all_sets_by_theme(LegoTheme theme, LegoSet set, LegoSet other)
     {
-        // TODO Use proper test data
-        LegoTheme theme = Context.Themes.First();
+        set.Theme = theme;
+        other.Theme = null;
+
+        Context.LegoSets.AddRange(set, other);
+        Context.SaveChanges();
 
         var expected = Context.LegoSets
             .Where(s => s.ThemeId == theme.Id)
@@ -26,14 +31,22 @@ public class SetControllerTest : DatabaseTest
 
         var actual = await controller.Get(theme.Id);
 
-        actual.Should().Contain(dto => expected.Contains(dto.SetNum));
+        actual.Should().HaveCount(1)
+            .And.ContainSingle(s => s.SetNum == set.SetNum)
+            .And.NotContain(s => s.SetNum == other.SetNum);
     }
 
-    [Fact]
-    public async Task Maps_to_SetDto()
+    [Theory, LegoAutoData]
+    public async Task Maps_to_SetDto(LegoTheme theme, LegoSet set)
     {
-        LegoTheme theme = Context.Themes.First();
-        LegoSet set = Context.LegoSets.First(s => s.ThemeId == theme.Id);
+        set.Theme = theme;
+
+        Context.LegoSets.Add(set);
+        Context.SaveChanges();
+
+        var result = await controller.Get(theme.Id);
+
+        LegoSetDto actual = result.Single(dto => dto.SetNum == set.SetNum);
 
         var expected = new LegoSetDto
         {
@@ -42,11 +55,7 @@ public class SetControllerTest : DatabaseTest
             Year = set.Year,
             NumParts = set.NumParts
         };
-
-        var result = await controller.Get(theme.Id);
-
-        LegoSetDto actual = result.Single(dto => dto.SetNum == set.SetNum);
-
+        
         actual.Should().Be(expected);
     }
 }
